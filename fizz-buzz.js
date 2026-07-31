862dd94075bd27b8b6db3b0a40aeda61f79a4142
flynnjustin24 <flynnjustin40@gmail.com>
2026-07-31 03:06:45 -0400

    Export fizzBuzz function and add CLI support

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,7 +1,20 @@
 // fizz-buzz.js
-for (let i = 1; i <= 100; i++) {
-  let out = '';
-  if (i % 3 === 0) out += 'Fizz';
-  if (i % 5 === 0) out += 'Buzz';
-  console.log(out || i);
+function fizzBuzz(n = 100) {
+  const out = [];
+  for (let i = 1; i <= n; i++) {
+    let s = '';
+    if (i % 3 === 0) s += 'Fizz';
+    if (i % 5 === 0) s += 'Buzz';
+    out.push(s || i);
+  }
+  return out;
 }
+
+// If run directly, print results to stdout (one per line)
+if (typeof require !== 'undefined' && require.main === module) {
+  const n = process.argv[2] ? Number(process.argv[2]) : 100;
+  console.log(fizzBuzz(n).join('\n'));
+}
+
+// Export for tests / other modules (CommonJS)
+if (typeof module !== 'undefined') module.exports = fizzBuzz;
